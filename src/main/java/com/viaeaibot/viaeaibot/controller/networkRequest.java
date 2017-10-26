efa805d3e8ba84cfc2e597740f65f623e73b3b51
tomide <tomideso@gmail.com>
2017-10-26 09:58:25 -0700

    refactor recipient

--- src/main/java/com/viaeaibot/viaeaibot/controller/networkRequest.java
+++ src/main/java/com/viaeaibot/viaeaibot/controller/networkRequest.java
@@ -104,7 +104,7 @@ public class networkRequest {
               
                     
                     System.out.println("Sending the message to facebook.");
-                    sendClient.publish("me/messages", GraphResponse.class, Parameter.with("recipient", item.getRecipient().getId()),
+                    sendClient.publish("me/messages", GraphResponse.class, Parameter.with("recipient", item.getSender().getId()),
                     Parameter.with("message", simpleMsg));
                 
                 
